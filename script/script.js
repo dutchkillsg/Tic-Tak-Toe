d86a8f2c4b589e4a2892a1952c874619bd1bb294
dutchkillsg <mip12793@gmail.com>
2017-08-01 16:11:30 -0400

    testing css and adding button to reset

--- script/script.js
+++ script/script.js
@@ -2,33 +2,38 @@
 $(document).ready(function(){
 	//create a player 1 to test
 	var player = 1;
+
 	//make a click event for the class sqaures
 	$('.square').on("click", function(event){
 		
 		//store the square class into $(this)
 		var squareSelected = $(this);
-		//make an if else statenent that adds the class "ex" || "oh" to your html to show it has been selected
-		if(squareSelected.hasClass("ex") || squareSelected.hasClass("oh")){
+		//make an if else statenent that adds the class "x" || "o" to your html to show it has been selected
+		if(squareSelected.hasClass("fa fa-times fa-4x") || squareSelected.hasClass("fa fa-circle-o fa-4x")){
 			alert("you have clicked here already!")
 		} else {
 			//when square has been selected by player 1 set the player to 2;vice versa
 			if(player===1){
-				squareSelected.addClass("ex");
-				if (checkWin("ex")){
+				squareSelected.addClass("fa fa-times fa-4x");
+				if (checkWin("fa fa-times fa-4x")){
 					alert("Congrats! Player " + player + " has WON!")
 				} else {
 				player = 2;
 				}
 			} else {
-				squareSelected.addClass("oh");
-				if (checkWin("oh")){
+				squareSelected.addClass("fa fa-circle-o fa-4x");
+				if (checkWin("fa fa-circle-o fa-4x")){
 					alert("Congrats! Player " + player + " has WON!")
 				} else {
 				player = 1;
+				if(player === player){
+					alert("its a draw")
+				}
 			 	}
 			}
 		}
 	});
+
 	//check if player won
 	//if player has 3 across,3 up/down, or 3 diagnal they win
 	function checkWin(symbol){
@@ -54,6 +59,10 @@ $(document).ready(function(){
 	}
 
 });
+
+
+
+
 
 	
 					
