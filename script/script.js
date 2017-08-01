3087c7b22f2cf9d0bf4bd4b9c8343ff2a9a053f7
dutchkillsg <mip12793@gmail.com>
2017-08-01 10:46:47 -0400

    added class selectors for x's and o's

--- script/script.js
+++ script/script.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
-
+	var player = 1;
 	$('.square').on("click", function(event){
-		alert("add symbol here")
+		
 
 		var sqaureSelected = $(this);
+		if(sqaureSelected.hasclass("ex") || sqaureSelected.hasclass("oh")){
+			alert("you have clicked here already!")
+		} else {
+			if(player===1){
+				sqaureSelected.addClass("ex");
+			} else {
+				sqaureSelected.addClass("oh");
+			}
+			//add the rest of our logic 
+		}
 	})
 
 });
