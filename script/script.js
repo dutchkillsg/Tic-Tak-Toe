71bf271021629d4cc9fe42eb048bd0aaf0179d2f
dutchkillsg <mip12793@gmail.com>
2017-08-01 11:33:10 -0400

    file needed a commit

--- script/script.js
+++ script/script.js
@@ -1,10 +1,30 @@
+//use doc.ready to wrap functions after page loads
 $(document).ready(function(){
-
+	//create a player 1 to test
+	var player = 1;
+	//make a click event for the class sqaures
 	$('.square').on("click", function(event){
-		alert("add symbol here")
-
-		var sqaureSelected = $(this);
-	})
+		
+		//store the square class into $(this)
+		var squareSelected = $(this);
+		//make an if else statenent that adds the class "ex" || "oh" to your html to show it has been selected
+		if(squareSelected.hasClass("ex") || squareSelected.hasClass("oh")){
+			alert("you have clicked here already!")
+		} else {
+			//when square has been selected by player 1 set the player to 2;vice versa
+			if(player===1){
+				squareSelected.addClass("ex");
+				player = 2;
+			} else {
+				squareSelected.addClass("oh");
+				player =1
+			}
+			//add the rest of our logic 
+		}
+	});
+	function checkWin(){
+		
+	}
 
 });
 
