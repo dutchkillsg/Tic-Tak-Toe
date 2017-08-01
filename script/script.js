4b5b2c836dd49234e79e8ae510a261fe8c1c22e4
dutchkillsg <mip12793@gmail.com>
2017-08-01 12:56:56 -0400

    added js and html to test

--- script/script.js
+++ script/script.js
@@ -1,10 +1,50 @@
+//use doc.ready to wrap functions after page loads
 $(document).ready(function(){
-
+	//create a player 1 to test
+	var player = 1;
+	//make a click event for the class sqaures
 	$('.square').on("click", function(event){
-		alert("add symbol here")
-
-		var sqaureSelected = $(this);
-	})
+		
+		//store the square class into $(this)
+		var squareSelected = $(this);
+		//make an if else statenent that adds the class "ex" || "oh" to your html to show it has been selected
+		if(squareSelected.hasClass("ex") || squareSelected.hasClass("oh")){
+			alert("you have clicked here already!")
+		} else {
+			//when square has been selected by player 1 set the player to 2;vice versa
+			if(player===1){
+				squareSelected.addClass("ex");
+				player = 2;
+			} else {
+				squareSelected.addClass("oh");
+				player =1
+			}
+			//add the rest of our logic 
+		}
+	});
+	//check if player won
+	//if player has 3 across,3 up/down, or 3 diagnal they win
+	function checkWin(symbol){
+		if($(".sq1").hasClass(symbol) && $(".sq2").hasClass(symbol) && $(".sq3").hasClass(symbol)){
+			return true
+		} else if($(".sq4").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq6").hasClass(symbol)){
+			return true
+		} else if($(".sq7").hasClass(symbol) && $(".sq8").hasClass(symbol) && $(".sq9").hasClass(symbol)){
+			return true
+		} else if($(".sq1").hasClass(symbol) && $(".sq4").hasClass(symbol) && $(".sq7").hasClass(symbol)){
+			return true
+		} else if($(".sq2").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq8").hasClass(symbol)){
+			return true
+		} else if($(".sq3").hasClass(symbol) && $(".sq6").hasClass(symbol) && $(".sq9").hasClass(symbol)){
+			return true
+		} else if($(".sq1").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq9").hasClass(symbol)){
+			return true
+		} else if($(".sq3").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq7").hasClass(symbol)){
+			return true
+		} else {
+			return false;
+		}
+	}
 
 });
 
