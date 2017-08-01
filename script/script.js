fdbf4ab8e93de91927f28df7543c7b846a2359b9
dutchkillsg <mip12793@gmail.com>
2017-08-01 11:52:44 -0400

    added checkWin function, now testing

--- script/script.js
+++ script/script.js
@@ -22,8 +22,28 @@ $(document).ready(function(){
 			//add the rest of our logic 
 		}
 	});
-	function checkWin(){
-		
+	//check if player won
+	//if player has 3 across,3 up/down, or 3 diagnal they win
+	function checkWin(symbol){
+		if($(".sq1").hasClass(symbol) && $(".sq2").hasClass(symbol) && $(".sq3").hasClass(symbol)){
+			return true
+		} else if($(".sq4").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq6").hasClass(symbol)){
+			return true
+		} else if($(".sq7").hasClass(symbol) && $(".sq8").hasClass(symbol) && $(".sq9").hasClass(symbol)){
+			return true
+		} else if($(".sq1").hasClass(symbol) && $(".sq4").hasClass(symbol) && $(".sq7").hasClass(symbol)){
+			return true
+		} else if($(".sq2").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq8").hasClass(symbol)){
+			return true
+		} else if($(".sq3").hasClass(symbol) && $(".sq6").hasClass(symbol) && $(".sq9").hasClass(symbol)){
+			return true
+		} else if($(".sq1").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq9").hasClass(symbol)){
+			return true
+		} else if($(".sq3").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq7").hasClass(symbol)){
+			return true
+		} else {
+			return false;
+		}
 	}
 
 });
