4d60015a50ff2262288221b2dda3a7bbe5495a56
dutchkillsg <mip12793@gmail.com>
2017-08-01 11:09:29 -0400

    added comments to break down script

--- script/script.js
+++ script/script.js
@@ -1,12 +1,17 @@
+//use doc.ready to wrap functions after page loads
 $(document).ready(function(){
+	//create a player 1 to test
 	var player = 1;
+	//make a click event for the class sqaures
 	$('.square').on("click", function(event){
 		
-
+		//store the square class into $(this)
 		var squareSelected = $(this);
+		//make an if else statenent that adds the class "ex" || "oh" to your html to show it has been selected
 		if(squareSelected.hasClass("ex") || squareSelected.hasClass("oh")){
 			alert("you have clicked here already!")
 		} else {
+			//when square has been selected by player 1 set the player to 2;vice versa
 			if(player===1){
 				squareSelected.addClass("ex");
 				player = 2;
