d6d823782c389e02258668d82dd65b30460ca3c1
dutchkillsg <mip12793@gmail.com>
2017-08-02 12:38:47 -0400

    working but no input player names functionality

--- script/script.js
+++ script/script.js
@@ -1,75 +1,13 @@
-/*//use doc.ready to wrap functions after page loads
-$(document).ready(function(){
-	//create a player 1 to test
-	var player = 1;
-
-	//make a click event for the class sqaures
-	$('.square').on("click", function(event){
-		
-		//store the square class into $(this)
-		var squareSelected = $(this);
-		//make an if else statenent that adds the class "x" || "o" to your html to show it has been already selected
-		if(squareSelected.hasClass("fa fa-times fa-4x") || squareSelected.hasClass("fa fa-circle-o fa-4x")){
-			alert("you have clicked here already!")
-		} else {
-			//when square has been selected by player 1 set the player to 2;vice versa
-			if(player===1){
-				squareSelected.addClass("fa fa-times fa-4x");
-				if (checkWin("fa fa-times fa-4x")){
-					alert("Congrats! Player " + player + " has WON!")
-				} else {
-				player = 2;
-				}
-			} else {
-				squareSelected.addClass("fa fa-circle-o fa-4x");
-				if (checkWin("fa fa-circle-o fa-4x")){
-					alert("Congrats! Player " + player + " has WON!")
-				} else {
-				player = 1;
-			
-			 	} 
-			}
-			//added a reset button that reloads the page 
-			$("#reset").on("click",function(){
-				location.reload();
-            });
-		}
-	});
-
-
-	//check if player won
-	//if player has 3 across,3 up/down, or 3 diagnal they win
-	function checkWin(symbol){
-		if($(".sq1").hasClass(symbol) && $(".sq2").hasClass(symbol) && $(".sq3").hasClass(symbol)){
-			return true
-		} else if($(".sq4").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq6").hasClass(symbol)){
-			return true
-		} else if($(".sq7").hasClass(symbol) && $(".sq8").hasClass(symbol) && $(".sq9").hasClass(symbol)){
-			return true
-		} else if($(".sq1").hasClass(symbol) && $(".sq4").hasClass(symbol) && $(".sq7").hasClass(symbol)){
-			return true
-		} else if($(".sq2").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq8").hasClass(symbol)){
-			return true
-		} else if($(".sq3").hasClass(symbol) && $(".sq6").hasClass(symbol) && $(".sq9").hasClass(symbol)){
-			return true
-		} else if($(".sq1").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq9").hasClass(symbol)){
-			return true
-		} else if($(".sq3").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq7").hasClass(symbol)){
-			return true
-		} else {
-         	return false
-
-		}
-	}
-
-});*/
-
 //scrapped the previous code becaause the CheckWin if else statement did not let the "tie" alert appear
-//directly put the "fa fa" icons into html so they dont need to be inserted with "add class"
+//directly put the "fa fa" icon classes into html so they dont need to be inserted with "add class"
 //stored player into a variable 
 //
 	
 $(document).ready(function(){
+	$("#submit").submit(function(){
+	    var names = [$("#player1").val(),$("#player2").val()]
+	}) // cant get the player names to work with the player variable. 
+    
     var player = 1;
     // Define the classes that are used for the two symbols
     var symbols = ['fa-times', 'fa-circle-o'];
